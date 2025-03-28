fdd7542a0ca3eac5340447b1af49081eee208205
jaheerman3312301105 <frdhn2323@gmail.com>
2025-03-28 23:14:03 +0700

    update draggable home

--- frontend/lib/pages/beranda.dart
+++ frontend/lib/pages/beranda.dart
@@ -1,8 +1,10 @@
+import 'package:draggable_home/draggable_home.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/widgets/berandawidgets/analisisberanda.dart';
 import 'package:frontend/widgets/berandawidgets/appbarberanda.dart';
 import 'package:frontend/widgets/berandawidgets/mapberanda.dart';
 import 'package:frontend/widgets/navigations/botnav.dart';
+import 'package:frontend/widgets/berandawidgets/listlaporanberanda.dart';
 
 class BerandaPages extends StatefulWidget {
   const BerandaPages({super.key});
@@ -15,18 +17,8 @@ class _BerandaPagesState extends State<BerandaPages> {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      bottomNavigationBar: BottomNavigation(),
-      appBar: AppBar(
-        title: SizedBox(
-          height: MediaQuery.of(context).size.height * 0.06,
-          child: AppBarBeranda()
-        ),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.045),
-          child: Column(
+    headerWidget () {
+      return Column(
             children: [
               SizedBox(
                 width: double.infinity,
@@ -40,8 +32,38 @@ class _BerandaPagesState extends State<BerandaPages> {
                   height: MediaQuery.of(context).size.height * 0.18,
                   child: AnalisisBeranda(),
                 ),
-              )
+              ),
             ],
+          );
+    }
+
+    return Scaffold(
+      bottomNavigationBar: BottomNavigation(),
+      appBar: AppBar(
+        title: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.06,
+          child: AppBarBeranda()
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.045),
+          child: DraggableHome(
+            centerTitle: true,
+            appBarColor: Colors.white,
+            headerExpandedHeight: 0.6,
+            title: Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: 4,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(75, 87, 103, 1),
+                borderRadius: BorderRadius.circular(20)
+              ),
+            ),
+            headerWidget: headerWidget(), 
+            body: [
+              ListLaporanBeranda()
+            ] 
           )
         ),
       )
---
+++ frontend/lib/widgets/berandawidgets/listlaporanberanda.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListLaporanBeranda extends StatefulWidget {
+  const ListLaporanBeranda({super.key});
+
+  @override
+  State<ListLaporanBeranda> createState() => _ListLaporanBerandaState();
+}
+
+class _ListLaporanBerandaState extends State<ListLaporanBeranda> {
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
